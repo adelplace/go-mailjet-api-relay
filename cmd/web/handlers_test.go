4514fe01a45189a1f87958c5ecc8f635f5304284
adelplace <alexandre.delplacemille@gmail.com>
2019-10-12 23:34:16 +0200

    Refacto tests

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -24,6 +24,26 @@ func sendEndpointRequest(app *application, is *is.I, method string, data *url.Va
 	return response, w
 }
 
+func newCpatchaMockHandler(success bool) func(w http.ResponseWriter, r *http.Request) {
+	status := "true"
+	if success == false {
+		status = "false"
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte(`{"success":` + status + `}`))
+	}
+}
+
+func mockCaptchaRequest(app *application, success bool) *httptest.Server {
+	mockHandler := newCpatchaMockHandler(success)
+	server := httptest.NewServer(http.HandlerFunc(mockHandler))
+	app.httpClient = server.Client()
+	app.reCaptchaURL = server.URL + "?a=%s&b=%s"
+
+	return server
+}
+
 func TestWrongMethod(t *testing.T) {
 	is := is.New(t)
 
@@ -48,24 +68,20 @@ func TestNoCaptcha(t *testing.T) {
 	is.Equal(response.Success, false)
 }
 
-func TestInvalidCaptchaFailed(t *testing.T) {
+func TestCheckCaptcha(t *testing.T) {
 	is := is.New(t)
-	mockHandler := func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte(`{"success":false}`))
-	}
 
-	server := httptest.NewServer(http.HandlerFunc(mockHandler))
-	defer server.Close()
-
+	app := newApplication()
 	data := &url.Values{}
 	data.Add("g-recaptcha-response", "fakeToken")
-	app := newApplication()
-	app.httpClient = server.Client()
-	app.reCaptchaURL = server.URL + "?a=%s&b=%s"
-	response, recorder := sendEndpointRequest(app, is, http.MethodPost, data)
 
-	is.Equal(response.Code, "invalid_captcha")
-	is.Equal(recorder.Code, http.StatusBadRequest)
-	is.Equal(response.Success, false)
+	serverSuccess := mockCaptchaRequest(app, false)
+	defer serverSuccess.Close()
+
+	is.Equal(app.checkCaptcha("fakeCaptcha"), false)
+
+	serverFail := mockCaptchaRequest(app, true)
+	defer serverFail.Close()
+
+	is.Equal(app.checkCaptcha("fakeCaptcha"), true)
 }
